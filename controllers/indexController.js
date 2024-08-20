9e283533ca15f00c286c7841199194d05bd6f91d
sinskiy <sinskiy@proton.me>
2024-08-20 14:29:17 +0300

    feat: display items

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,8 @@
-function indexGet(req, res) {
-  res.render("index");
+const db = require("../db/queries");
+
+async function indexGet(req, res) {
+  const { rows } = await db.getAllItems();
+  res.render("index", { items: rows });
 }
 
 module.exports = { indexGet };
--- db/queries.js
+++ db/queries.js
@@ -1,1 +1,7 @@
 const pool = require("./pool");
+
+async function getAllItems() {
+  return await pool.query("SELECT * FROM items");
+}
+
+module.exports = { getAllItems };
